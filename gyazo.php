259adfafb85b693cb94258c35d4b2b7d5c14d7cc
moocher <moocher@moocher.ru>
2015-09-28 13:04:52 +0300

    Init

---
+++ gyazo.php
@@ -0,0 +1,7 @@
+<?php
+echo "http://moocher.ru/";
+if(isset($_FILES['imagedata']['name'])) {
+    $path = 'gyazo/' . substr(md5(time()), -28) . '.png';
+    if(move_uploaded_file($_FILES['imagedata']['tmp_name'], $path))
+        echo $path;
+}
