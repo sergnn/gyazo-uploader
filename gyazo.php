41eeef0dea718e04cdeb12beb485a0b20cfbeb83
Sergey Fomin <moocher@moocher.ru>
2015-09-28 13:05:33 +0300

    Update gyazo.php

--- gyazo.php
+++ gyazo.php
@@ -1,5 +1,5 @@
 <?php
-echo "http://moocher.ru/";
+echo "URL";
 if(isset($_FILES['imagedata']['name'])) {
     $path = 'gyazo/' . substr(md5(time()), -28) . '.png';
     if(move_uploaded_file($_FILES['imagedata']['tmp_name'], $path))
